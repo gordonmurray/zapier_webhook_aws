ea45f1e32791b4650b48bdad31a7072323fcd77b
Gordon Murray <gordon@gordonmurray.com>
2020-01-11 18:04:21 +0000

    alb and target group

---
+++ terraform/alb.tf
@@ -0,0 +1,26 @@
+resource "aws_lb" "loadbalancer" {
+  name               = "application-loadbalancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.application.id]
+  subnets            = [aws_subnet.subnet-1a.id, aws_subnet.subnet-1b.id]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = "aws_s3_bucket.zapier_webhook_loadbalancer_logs.bucket"
+    prefix  = ""
+    enabled = true
+  }
+
+  tags = {
+    Name = var.application_name
+  }
+}
+
+resource "aws_lb_target_group" "application_targetgroup" {
+  name     = "application-targetgroup"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "aws_vpc.application.id"
+}
