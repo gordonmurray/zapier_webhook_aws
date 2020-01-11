3a5dcdbe9f81bf8e67710e24b16c55c944b63b79
Gordon Murray <gordon@gordonmurray.com>
2020-01-11 21:14:30 +0000

    certificate dns validation

--- terraform/certificate.tf
+++ terraform/certificate.tf
@@ -11,3 +11,16 @@ resource "aws_acm_certificate" "gordonmurray" {
     create_before_destroy = true
   }
 }
+
+resource "aws_route53_record" "cert_validation" {
+  name    = "${aws_acm_certificate.gordonmurray.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.gordonmurray.domain_validation_options.0.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.application_zone.id}"
+  records = ["${aws_acm_certificate.gordonmurray.domain_validation_options.0.resource_record_value}"]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = "${aws_acm_certificate.gordonmurray.arn}"
+  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+}
