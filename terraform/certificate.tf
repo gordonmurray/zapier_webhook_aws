d0f4c23bc02975caedf0f6f2d678e64025941430
Gordon Murray <gordon@gordonmurray.com>
2020-01-11 20:47:46 +0000

    list of alternate cert names

--- terraform/certificate.tf
+++ terraform/certificate.tf
@@ -1,7 +1,7 @@
 resource "aws_acm_certificate" "gordonmurray" {
-  domain_name       = "gordonmurray.com"
-  validation_method = "DNS"
-  subject_alternative_names = "www.gordonmurray.com"
+  domain_name               = "gordonmurray.com"
+  validation_method         = "DNS"
+  subject_alternative_names = ["www.gordonmurray.com"]
 
   tags = {
     Name = var.application_name
