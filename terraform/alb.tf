a5f7f8d2a1a94e495ca3f99b29743db8a935d5f7
Gordon Murray <gordon@gordonmurray.com>
2020-01-11 23:00:56 +0000

    variable instead of a string

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -28,7 +28,7 @@ resource "aws_lb_listener" "application" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-  certificate_arn   = "aws_acm_certificate.gordonmurray.arn"
+  certificate_arn   = aws_acm_certificate.gordonmurray.arn
 
   default_action {
     type             = "forward"
