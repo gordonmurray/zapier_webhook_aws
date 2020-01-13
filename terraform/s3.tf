d480dd7064306db8dfaa2ce51514cc897b157fa0
Gordon Murray <gordon@gordonmurray.com>
2020-01-13 21:04:45 +0000

    s3 lifecycle

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,6 +6,28 @@ resource "aws_s3_bucket" "zapier_webhook_logs" {
   tags = {
     Name = var.application_name
   }
+
+  lifecycle_rule {
+
+    enabled = true
+
+    prefix = ""
+
+    tags = {
+      Name = var.application_name
+    }
+
+    transition {
+      days          = 30
+      storage_class = "ONEZONE_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+  }
 }
 
 resource "aws_s3_bucket" "zapier_webhook_loadbalancer_logs" {
